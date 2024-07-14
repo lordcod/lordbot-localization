7f8461ca1ed0125eb6b33609acb5504cc26fa8ab
LordCode <9999269010dddd@gmail.com>
2024-07-15 01:43:09 +0300

    upd translate

--- translator.py
+++ translator.py
@@ -9,6 +9,11 @@ for lang in  languages:
         for data in orjson.loads(file.read()):
             key = data['identifier'].strip('"')
             localization_data.setdefault(lang, {})
+            if key in localization_data[lang]:
+                print('1:', localization_data[lang][key])
+                print('2:', data['translation'])
+                if input('> ') == 1:
+                    continue
             localization_data[lang][key] = data['translation']
 
 open('any_localization.json', 'wb').write(orjson.dumps(localization_data))
